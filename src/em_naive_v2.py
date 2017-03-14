d88913fec9bfd322dd8c7dfebfc5fe91c42ffd47
Avinash Ramu <aramu@genome.wustl.edu>
2017-03-14 15:53:06 -0500

    Update EM algorithm

--- src/em_naive_v2.py
+++ src/em_naive_v2.py
@@ -1,7 +1,9 @@
 import sys
 import csv
 import math
+import random
 import scipy.stats
+import statistics
 from math import log as log
 
 class NaiveEM:
@@ -39,65 +41,76 @@ class NaiveEM:
             print("x", x)
             print("sigmasq", self.sigmasq)
             print("q", self.q)
+            print("1-q", 1 - self.q)
             print("mu1", self.mu1)
             print("mu2", self.mu2)
             print("mu3", self.mu3)
+            print("fractions: ", log(1.0 - self.q) + log(1.0 - self.q), log(2) + log(self.q) + log(1.0 - self.q), log(self.q) + log(self.q))
             #Hom Ref
             aalik = log(1.0 - self.q) + log(1.0 - self.q) + scipy.stats.norm.logpdf(x, self.mu1, self.sigma)
             #Het
-            ablik = log(2 * self.q * (1.0 - self.q)) + scipy.stats.norm.logpdf(x, self.mu2, self.sigma)
+            ablik = log(2) + log(self.q) + log(1.0 - self.q) + scipy.stats.norm.logpdf(x, self.mu2, self.sigma)
             #Hom Alt
             bblik = log(self.q) + log(self.q) + scipy.stats.norm.logpdf(x, self.mu3, self.sigma)
-            print(aalik, ablik, bblik)
+            print("liks", aalik, ablik, bblik)
+            print(scipy.stats.norm.logpdf(x, self.mu1, self.sigma))
             aalik_norm = math.exp(aalik)
             ablik_norm = math.exp(ablik)
             bblik_norm = math.exp(bblik)
-            print(aalik_norm, ablik_norm, bblik_norm)
-            sumlik_norm = self.mu1 * aalik_norm + self.mu2 * ablik_norm + self.mu3 * bblik_norm
+            print("after exp", aalik_norm, ablik_norm, bblik_norm)
+            sumlik_norm = aalik_norm + ablik_norm + bblik_norm
             print("sumlik_norm", aalik_norm + ablik_norm + bblik_norm)
+            self.gtsaa[i] = math.exp(math.log(aalik_norm) - math.log(sumlik_norm))
+            self.gtsab[i] = math.exp(math.log(ablik_norm) - math.log(sumlik_norm))
+            self.gtsbb[i] = math.exp(math.log(bblik_norm) - math.log(sumlik_norm))
+            self.n1 += self.gtsaa[i]
+            self.n2 += self.gtsab[i]
+            self.n3 += self.gtsbb[i]
+            """
+            Ignore for now
             if sumlik_norm == 0:
+                print("Inside norm")
                 aalik_norm = 1.0/3
                 ablik_norm = 1.0/3
                 bblik_norm = 1.0/3
             else:
-                aalik_norm = (self.mu1 * aalik_norm)/sumlik_norm
-                ablik_norm = (self.mu2 * ablik_norm)/sumlik_norm
-                bblik_norm = (self.mu3 * bblik_norm)/sumlik_norm
-            #Temporary
-            aalik_norm = aalik
-            ablik_norm = ablik
-            bblik_norm = bblik
-
-            print(aalik_norm, ablik_norm, bblik_norm)
-            print(aalik_norm + ablik_norm + bblik_norm)
-
+                aalik_norm = math.log(aalik_norm) - math.log(sumlik_norm)
+                ablik_norm = math.log(ablik_norm) - math.log(sumlik_norm)
+                bblik_norm = math.log(bblik_norm) - math.log(sumlik_norm)
             self.gtsaa[i] = 0
             self.gtsab[i] = 0
             self.gtsbb[i] = 0
-            if aalik_norm > ablik_norm and aalik_norm > bblik_norm:
+            if aalik_norm >= ablik_norm and aalik_norm >= bblik_norm:
                 self.gtsaa[i] = 1.0
                 self.n1 += 1
-            if ablik_norm > aalik_norm and ablik_norm > bblik_norm:
+            elif ablik_norm >= aalik_norm and ablik_norm >= bblik_norm:
                 self.gtsab[i] = 1.0
                 self.n2 += 1
-            if bblik_norm > aalik_norm and bblik_norm > ablik_norm:
+            elif bblik_norm >= aalik_norm and bblik_norm >= ablik_norm:
                 self.gtsbb[i] = 1.0
                 self.n3 += 1
+            else:
+                raise("Invalid genotypes " + str(aalik_norm) + "," + str(ablik_norm) + "," + str(bblik_norm))
+            """
             print("gt[i]", self.gtsaa[i], self.gtsab[i], self.gtsbb[i])
-        print("gt", self.gtsaa, self.gtsab, self.gtsbb, len(self.gtsaa), len(self.gtsab))
+        print("gt", self.gtsaa, self.gtsab, self.gtsbb)
+        print("gtsum", [x + y + z for x, y, z in zip(self.gtsaa, self.gtsab, self.gtsbb)])
 
     def m_step(self):
         n = len(self.qts)
+        n = (self.n1 + self.n2 + self.n3)
         #Total sum for each genotype
         aasum = 0
         absum = 0
         bbsum = 0
         self.q = 0
         for i, x in enumerate(self.qts):
+            print("i", i, "x", x, "gts", self.gtsaa[i], self.gtsab[i], self.gtsbb[i])
             aasum += x * self.gtsaa[i]
             absum += x * self.gtsab[i]
             bbsum += x * self.gtsbb[i]
             self.q += 2 * self.gtsbb[i] + self.gtsab[i]
+        print("aasum", "absub", "bbsum", aasum, absum, bbsum, "n1, n2, n3", self.n1, self.n2, self.n3)
         self.q = self.q/(2 * n) + sys.float_info.min
         if self.n1 == 0:
             self.mu1 = 0
@@ -113,30 +126,30 @@ class NaiveEM:
             self.mu3 = bbsum/(self.n3)
         self.sigmanew = 0
         for i, x in enumerate(self.qts):
-            self.sigmanew += ((x * self.gtsaa[i]) ** 2 - (x * self.gtsaa[i]))
-            self.sigmanew += ((x * self.gtsab[i]) ** 2 - (x * self.gtsab[i]))
-            self.sigmanew += ((x * self.gtsbb[i]) ** 2 - (x * self.gtsbb[i]))
-        print("sigmanew", self.sigmanew)
+            self.sigmanew += ((x - self.mu1) * self.gtsaa[i]) ** 2
+            self.sigmanew += ((x - self.mu2) * self.gtsab[i]) ** 2
+            self.sigmanew += ((x - self.mu3) * self.gtsbb[i]) ** 2
         self.sigma = math.sqrt(self.sigmanew/(n-1))
-        self.sigma = 0.1
-        self.q = 0.5
+        print("sigma calculated", self.sigma)
+        #self.sigma = 1
         print("sigma", self.sigma)
         print("qnew", self.q)
         print("gts", sum(self.gtsaa), sum(self.gtsab), sum(self.gtsbb))
 
     def run_em(self):
         #Initial condition
-        self.mu1 = 10
-        self.mu2 = 10
-        self.mu3 = 10
-        self.q = 0.5
-        self.sigma = 0.1
-        for i in range(10000):
+        self.mu1 = self.qts[random.randint(0, len(self.qts) - 1)]
+        self.mu2 = self.qts[random.randint(0, len(self.qts) - 1)]
+        self.mu3 = self.qts[random.randint(0, len(self.qts) - 1)]
+        self.sigma = 10
+        self.q = 0.1
+        for i in range(1000):
+            print([self.mu1, self.mu2, self.mu3, self.sigma, self.q])
+            print("Iteration: ", i+1)
             print("Running E step")
             self.e_step()
             print("Running M step")
             self.m_step()
-            print([self.mu1, self.mu2, self.mu3, self.sigma, self.q])
 
 def main():
     qt_file = sys.argv[1]
