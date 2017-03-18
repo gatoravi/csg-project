1da330f3ea9b0a65e75f22786e7bf4438c4a0d6b
Avinash Ramu <aramu@genome.wustl.edu>
2017-03-17 23:30:59 -0500

    Update EM

--- src/em_naive_v2.py
+++ src/em_naive_v2.py
@@ -2,6 +2,7 @@ import sys
 import csv
 import math
 import random
+import numpy as np
 import scipy.stats
 import statistics
 from math import log as log
@@ -16,6 +17,7 @@ class NaiveEM:
     qts = []
 
     def __init__(self, qt_file):
+        "Initialize all the params"
         self.total_like_new = 0
         self.total_like_old = 0
         with open(qt_file) as qtfh:
@@ -37,22 +39,13 @@ class NaiveEM:
         self.total_like_old = self.total_like_new
         self.total_like_new = 0
         for i, x in enumerate(self.qts):
-            """
-            print("x", x)
-            print("sigmasq", self.sigmasq)
-            print("q", self.q)
-            print("1-q", 1 - self.q)
-            print("mu1", self.mu1)
-            print("mu2", self.mu2)
-            print("mu3", self.mu3)
-            print("fractions: ", log(1.0 - self.q) + log(1.0 - self.q), log(2) + log(self.q) + log(1.0 - self.q), log(self.q) + log(self.q))
-            """
             #Hom Ref
             aalik = log(1.0 - self.q) + log(1.0 - self.q) + scipy.stats.norm.logpdf(x, self.mu1, self.sigma)
             #Het
             ablik = log(2) + log(self.q) + log(1.0 - self.q) + scipy.stats.norm.logpdf(x, self.mu2, self.sigma)
             #Hom Alt
             bblik = log(self.q) + log(self.q) + scipy.stats.norm.logpdf(x, self.mu3, self.sigma)
+            """
             aalik_new = aalik - max(aalik, ablik, bblik)
             ablik_new = ablik - max(aalik, ablik, bblik)
             bblik_new = bblik - max(aalik, ablik, bblik)
@@ -65,50 +58,25 @@ class NaiveEM:
                 ablik = -100
             if bblik < -100:
                 bblik = -100
+            """
             #print(scipy.stats.norm.logpdf(x, self.mu1, self.sigma))
             aalik_norm = math.exp(aalik)
             ablik_norm = math.exp(ablik)
             bblik_norm = math.exp(bblik)
             #print("after exp", aalik_norm, ablik_norm, bblik_norm)
             sumlik_norm = aalik_norm + ablik_norm + bblik_norm
-            #print("sumlik_norm", aalik_norm + ablik_norm + bblik_norm)
             self.gtsaa[i] = math.exp(math.log(aalik_norm) - math.log(sumlik_norm))
             self.gtsab[i] = math.exp(math.log(ablik_norm) - math.log(sumlik_norm))
             self.gtsbb[i] = math.exp(math.log(bblik_norm) - math.log(sumlik_norm))
             self.n1 += self.gtsaa[i]
             self.n2 += self.gtsab[i]
             self.n3 += self.gtsbb[i]
-            self.total_like_new += math.log(math.exp(aalik) + math.exp(ablik) + math.exp(bblik))
-            """
-            Ignore for now
-            if sumlik_norm == 0:
-                print("Inside norm")
-                aalik_norm = 1.0/3
-                ablik_norm = 1.0/3
-                bblik_norm = 1.0/3
-            else:
-                aalik_norm = math.log(aalik_norm) - math.log(sumlik_norm)
-                ablik_norm = math.log(ablik_norm) - math.log(sumlik_norm)
-                bblik_norm = math.log(bblik_norm) - math.log(sumlik_norm)
-            self.gtsaa[i] = 0
-            self.gtsab[i] = 0
-            self.gtsbb[i] = 0
-            if aalik_norm >= ablik_norm and aalik_norm >= bblik_norm:
-                self.gtsaa[i] = 1.0
-                self.n1 += 1
-            elif ablik_norm >= aalik_norm and ablik_norm >= bblik_norm:
-                self.gtsab[i] = 1.0
-                self.n2 += 1
-            elif bblik_norm >= aalik_norm and bblik_norm >= ablik_norm:
-                self.gtsbb[i] = 1.0
-                self.n3 += 1
-            else:
-                raise("Invalid genotypes " + str(aalik_norm) + "," + str(ablik_norm) + "," + str(bblik_norm))
-            """
-            #print("gt[i]", self.gtsaa[i], self.gtsab[i], self.gtsbb[i])
+            self.total_like_new += math.log(sumlik_norm)
         #print("gt", self.gtsaa, self.gtsab, self.gtsbb)
         #print("gtsum", [x + y + z for x, y, z in zip(self.gtsaa, self.gtsab, self.gtsbb)])
-        print("liks", self.total_like_old, self.total_like_new, self.total_like_new - self.total_like_old)
+        self.like_diff = self.total_like_new - self.total_like_old
+        print("Likelihoods - old, new, new - old: ", self.total_like_old, self.total_like_new, self.like_diff)
+        print("n1, n2, n3: ", self.n1, self.n2, self.n3)
 
     def m_step(self):
         n = len(self.qts)
@@ -119,15 +87,10 @@ class NaiveEM:
         bbsum = 0
         self.q = 0
         for i, x in enumerate(self.qts):
-            #print("i", i, "x", x, "gts", self.gtsaa[i], self.gtsab[i], self.gtsbb[i])
             aasum += x * self.gtsaa[i]
             absum += x * self.gtsab[i]
             bbsum += x * self.gtsbb[i]
-            gtsum = self.gtsaa[i] + self.gtsab[i] + self.gtsbb[i]
-            if gtsum < 0.95 or gtsum > 1.05:
-                print("gtsum", gtsum)
-            self.q += 2 * self.gtsbb[i] + self.gtsab[i]
-        print("aasum", "absub", "bbsum", aasum, absum, bbsum, "n1, n2, n3", self.n1, self.n2, self.n3)
+            self.q += 2.0 * self.gtsbb[i] + self.gtsab[i]
         self.q = self.q/(2 * n)
         if self.n1 == 0:
             self.mu1 = 0
@@ -147,8 +110,6 @@ class NaiveEM:
             self.sigmanew += ((x - self.mu2)) ** 2 * self.gtsab[i]
             self.sigmanew += ((x - self.mu3)) ** 2 * self.gtsbb[i]
         self.sigma = math.sqrt(self.sigmanew/n)
-        print("sigma", self.sigma)
-        print("qnew", self.q)
         print("gts", sum(self.gtsaa), sum(self.gtsab), sum(self.gtsbb))
 
     def run_em(self):
@@ -156,17 +117,22 @@ class NaiveEM:
         self.mu1 = self.qts[random.randint(0, len(self.qts) - 1)]
         self.mu2 = self.qts[random.randint(0, len(self.qts) - 1)]
         self.mu3 = self.qts[random.randint(0, len(self.qts) - 1)]
-        self.sigma = 10
-        self.q = 0.01
+        self.sigma = np.std(self.qts)
+        self.q = 0.5
+        print("Initial params mus, sigma, q", [self.mu1, self.mu2, self.mu3, self.sigma, self.q])
         for i in range(1000):
-            print("params mus, sigma, q", [self.mu1, self.mu2, self.mu3, self.sigma, self.q])
-            print("Iteration: ", i+1)
+            print("\nIteration: ", i+1)
             print("Running E step")
             self.e_step()
             print("Running M step")
             self.m_step()
+            print("Params mus, sigma, q", [self.mu1, self.mu2, self.mu3, self.sigma, self.q])
+            if i != 0 and self.like_diff < 1e-12:
+                print("\nLikelihood difference less than 1e-12. Stopping EM.", self.like_diff)
+                break
 
 def main():
+    "Everything happens here"
     qt_file = sys.argv[1]
     em1 = NaiveEM(qt_file)
     em1.run_em()
